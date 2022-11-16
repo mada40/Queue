7a360bb39b9c938ef5f15c7aa3b5e0f422906818
4mada0 <4mada0@gmail.com>
2022-11-16 11:45:59 +0300

    release

--- include/tqueue.h
+++ include/tqueue.h
@@ -1,11 +1,7 @@
 ﻿#ifndef __TDynamicMatrix_H__
 #define __TDynamicMatrix_H__
 
-#include <iostream>
-
 using namespace std;
-
-const int MAX_QUEUE_SIZE = 100000000;
 
 
 template<typename T>
@@ -13,45 +9,42 @@ class TDynamicQueue
 {
 
 protected:
-	static const int COEFF = 2;
 	size_t sz;
 	size_t memSize;
 	T* pMem;
 	T* head, *tail;
 
 public:
-	TDynamicQueue() : memSize()
+	static const int COEFF = 2;
+	TDynamicQueue()
 	{
 		sz = 0;
-		memSize = 2 * COEFF;
-		pMem = new T[memSize]();// {}; // У типа T д.б. констуктор по умолчанию
+		memSize = 0;
+		pMem = nullptr;
 		head = pMem;
-		tail = pMem + 1;
+		tail = pMem;
 	}
 
 	TDynamicQueue(const TDynamicQueue& q)
 	{
 		sz = q.sz;
-		pMem = new T[sz];
+		memSize = q.memSize;
+
+		pMem = new T[memSize];
 		head = pMem;
-		tail = pMem + 1;
+		tail = pMem + sz;
 		std::copy(q.pMem, q.pMem + sz, pMem);
 	}
-	TDynamicQueue(TDynamicQueue&& v) noexcept
-	{
-		sz = v.sz;
-		memSize = v.memSize;
-		pMem = v.pMem;
-		head = v.head;
-		tail = v.tail;
-
-		v.sz = 0;
-		memSize = 0;
-		v.pMem = v.head = v.tail = nullptr;
-	}
+	
 	~TDynamicQueue()
 	{
 		if (pMem) delete[] pMem;
+	}
+
+	friend void swap(TDynamicQueue& lhs, TDynamicQueue& rhs) noexcept
+	{
+		std::swap(lhs.sz, rhs.sz);
+		std::swap(lhs.pMem, rhs.pMem);
 	}
 	TDynamicQueue& operator=(const TDynamicQueue& v)
 	{
@@ -62,14 +55,10 @@ public:
 		swap(*this, tmp);
 		return *this;
 	}
-	TDynamicQueue& operator=(TDynamicQueue&& v) noexcept
-	{
-		swap(*this, v);
-		return *this;
-	}
 
 	size_t size() const noexcept { return sz; }
-
+	size_t capacity() const noexcept{ return memSize; }
+	bool empty() const noexcept { return sz == 0; }
 
 	
 	// сравнение
@@ -91,23 +80,17 @@ public:
 		return !(*this == v);
 	}
 
-	friend void swap(TDynamicQueue& lhs, TDynamicQueue& rhs) noexcept
-	{
-		std::swap(lhs.sz, rhs.sz);
-		std::swap(lhs.pMem, rhs.pMem);
-	}
-
-	const T& front() 
+	T front() const
 	{
 		if (sz == 0)
-			out_of_range("queue is empty");
+			throw out_of_range("queue is empty");
 
 		return *head;
 	}
 	void pop() 
 	{ 
 		if (sz == 0)
-			out_of_range("queue is empty");
+			throw out_of_range("queue is empty");
 
 		head = pMem + (head - pMem + 1) % memSize;
 		sz--; 
@@ -118,11 +101,11 @@ public:
 
 		if (tail == head)
 		{
-			size_t newSize = memSize * COEFF;
+			size_t newSize = (memSize + 1) * COEFF;
 			T* tmp = new T[newSize];
 			memSize = newSize;
 			std::copy(pMem, pMem + sz, tmp);
-			delete pMem;
+			if(pMem) delete[] pMem;
 			pMem = tmp;
 			head = pMem;
 			tail = pMem + sz;
@@ -132,6 +115,8 @@ public:
 		tail = pMem + (tail - pMem + 1) % memSize;
 		sz++;
 	}
+
+
 
 
 };
--- test/test_tqueue.cpp
+++ test/test_tqueue.cpp
@@ -2,190 +2,118 @@
 
 #include <gtest.h>
 
-TEST(TDynamicQueue, can_create_matrix_with_positive_length)
+TEST(TDynamicQueue, can_create)
+{
+	ASSERT_NO_THROW(TDynamicQueue<int> q);
+}
+
+TEST(TDynamicQueue, can_push)
 {
 	TDynamicQueue<int> q;
-	for (int i = 0; i < 10; ++i)
+	ASSERT_NO_THROW(q.push(5));
+}
+
+TEST(TDynamicQueue, cant_pop_empty_queue)
+{
+	TDynamicQueue<int> q;
+	ASSERT_ANY_THROW(q.pop());
+}
+
+TEST(TDynamicQueue, can_pop_queue)
+{
+	TDynamicQueue<int> q;
+	q.push(5);
+	ASSERT_NO_THROW(q.pop());
+}
+
+TEST(TDynamicQueue, front_is_equal_to_first_in)
+{
+	TDynamicQueue<int> q;
+	int EL = 5;
+	q.push(EL);
+	int tmp = q.front();
+	EXPECT_EQ(tmp, EL);
+}
+
+TEST(TDynamicQueue, can_check_is_empty)
+{
+	TDynamicQueue<int> q;
+	EXPECT_EQ(q.empty(), true);
+}
+
+TEST(TDynamicQueue, can_get_size)
+{
+	TDynamicQueue<int> q;
+	for (int i = 0; i < 100; i++)
+		q.push(2 * i + 100);
+	EXPECT_EQ(q.size(), 100);
+}
+
+TEST(TDynamicQueue, can_create_copied_queue)
+{
+	TDynamicQueue<int> q;
+	q.push(580);
+	q.push(550);
+	q.pop();
+	q.push(32);
+	ASSERT_NO_THROW(TDynamicQueue<int> q1(q));
+}
+
+TEST(TDynamicQueue, copied_matrix_is_equal_to_source_one)
+{
+	TDynamicQueue<int> q;
+	q.push(580);
+	q.push(550);
+	q.pop();
+	q.push(32);
+	TDynamicQueue<int> q1(q);
+	EXPECT_EQ(q1, q);
+}
+	
+TEST(TDynamicQueue, copied_queue_has_its_own_memory)
+{
+	TDynamicQueue<int> q1;
+	q1.push(5);
+	TDynamicQueue<int> q2(q1);
+	q2.pop(); q2.push(58);
+	int tm1 = q1.front();
+	int tm2 = q2.front();
+	EXPECT_NE(q1.front(), q2.front());
+}
+
+TEST(TDynamicQueue, can_assign_queue_to_itself)
+{
+	TDynamicQueue<int> q;
+	q.push(580);
+	q.pop();
+	q.push(32);
+	ASSERT_NO_THROW(q = q);
+}
+
+TEST(TDynamicQueue, pop_reduces_size)
+{
+	TDynamicQueue<int> q;
+	q.push(580);
+	q.push(585);
+	q.push(589);
+	q.pop();
+	EXPECT_EQ(q.size(), 2);
+}
+
+TEST(TDynamicQueue, loop_1push1pop_after_100pop)
+{
+	TDynamicQueue<int> q;
+	const int N = 100;
+	for (int i = 0; i < N; i++)
 	{
-		q.push(i * 2 + 25);
+		q.push(i - 100);
 	}
+	for (int i = 0; i < 1e7; i++)
+	{
+		q.push(i * 2 + 1);
+		q.pop();
+	}
+	EXPECT_LE(q.capacity(), N * q.COEFF);
 }
 
-/*
 
-TEST(TDynamicMatrix, can_create_matrix_with_positive_length)
-{
-	ASSERT_NO_THROW(TDynamicMatrix<int> m(5));
-}
-
-TEST(TDynamicMatrix, cant_create_too_large_matrix)
-{
-	ASSERT_ANY_THROW(TDynamicMatrix<int> m(MAX_MATRIX_SIZE + 1));
-}
-
-TEST(TDynamicMatrix, throws_when_create_matrix_with_negative_length)
-{
-	ASSERT_ANY_THROW(TDynamicMatrix<int> m(-5));
-}
-
-TEST(TDynamicMatrix, can_create_copied_matrix)
-{
-  TDynamicMatrix<int> m(5);
-
-  ASSERT_NO_THROW(TDynamicMatrix<int> m1(m));
-}
-
-TEST(TDynamicMatrix, copied_matrix_is_equal_to_source_one)
-{
-	TDynamicMatrix<int> m1(2);
-	m1[0][1] = 2;
-	TDynamicMatrix<int> m2(m1);
-	EXPECT_EQ(m1, m2);
-
-}
-
-TEST(TDynamicMatrix, copied_matrix_has_its_own_memory)
-{
-	TDynamicMatrix<int> m1(2);
-	m1[0][1] = 2;
-	TDynamicMatrix<int> m2(m1);
-	m1[1][1] = 99;
-	EXPECT_NE(m1, m2);
-}
-
-TEST(TDynamicMatrix, can_get_size)
-{
-	TDynamicMatrix<int> m1(2);
-	int size = m1.size();
-	EXPECT_EQ(size, 2);
-}
-
-TEST(TDynamicMatrix, can_set_and_get_element)
-{
-	TDynamicMatrix<int> m1(2);
-	m1[1][0] = 99;
-	EXPECT_EQ(99, m1[1][0]);
-}
-
-TEST(TDynamicMatrix, throws_when_set_element_with_negative_index)
-{
-	TDynamicMatrix<int> m1(2);
-	ASSERT_ANY_THROW(m1.at(-1, 0));
-	ASSERT_ANY_THROW(m1.at(0, -1));
-}
-
-TEST(TDynamicMatrix, throws_when_set_element_with_too_large_index)
-{
-	TDynamicMatrix<int> m1(2);
-	ASSERT_ANY_THROW(m1.at(3, 0));
-	ASSERT_ANY_THROW(m1.at(0, 3));
-}
-
-TEST(TDynamicMatrix, can_assign_matrix_to_itself)
-{
-	TDynamicMatrix<int> m(4);
-	ASSERT_NO_THROW(m = m);
-
-}
-
-TEST(TDynamicMatrix, can_assign_matrices_of_equal_size)
-{
-	TDynamicMatrix<int> m1(4), m2(4);
-	m1[0][0] = 5;
-	m2 = m1;
-	EXPECT_EQ(m1, m2);
-}
-
-TEST(TDynamicMatrix, assign_operator_change_matrix_size)
-{
-	TDynamicMatrix<int> m1(4), m2(5);
-	m1[0][0] = 5;
-	m2 = m1;
-	EXPECT_EQ(m2.size(), 4);
-}
-
-TEST(TDynamicMatrix, can_assign_matrices_of_different_size)
-{
-	TDynamicMatrix<int> m1(4), m2(5);
-	m1[0][0] = 5;
-	m2 = m1;
-	EXPECT_EQ(m1, m2);
-}
-
-TEST(TDynamicMatrix, compare_equal_matrices_return_true)
-{
-	TDynamicMatrix<int> m1(4), m2(4);
-	m1[0][1] = 5;
-	m2 = m1;
-	EXPECT_EQ(m1, m2);
-}
-
-TEST(TDynamicMatrix, compare_matrix_with_itself_return_true)
-{
-	TDynamicMatrix<int> m1(4), m2(4);
-	m1[0] = 5;
-	EXPECT_EQ(m1, m1);
-}
-
-TEST(TDynamicMatrix, matrices_with_different_size_are_not_equal)
-{
-	TDynamicMatrix<int> m1(4), m2(5);
-	EXPECT_NE(m1, m2);
-}
-
-TEST(TDynamicMatrix, can_add_matrices_with_equal_size)
-{
-	TDynamicMatrix<int> m1(2), m2(2);
-	m1[0][0] = 1;
-	m1[1][1] = 6;
-
-	m2[1][1] = -1;
-	TDynamicMatrix<int> res(2);
-	res[0][0] = 1;
-	res[1][1] = 5;
-	EXPECT_EQ(res, m1 + m2);
-}
-
-TEST(TDynamicMatrix, cant_add_matrices_with_not_equal_size)
-{
-	TDynamicMatrix<int> m1(2), m2(3);
-	ASSERT_ANY_THROW(m1 + m2);
-}
-
-TEST(TDynamicMatrix, can_subtract_matrices_with_equal_size)
-{
-	TDynamicMatrix<int> m1(2), m2(2);
-	m1[0][0] = 1;
-	m1[1][1] = 6;
-
-	m2[1][1] = -1;
-	TDynamicMatrix<int> res(2);
-	res[0][0] = 1;
-	res[1][1] = 7;
-	EXPECT_EQ(res, m1 - m2);
-}
-
-TEST(TDynamicMatrix, cant_subtract_matrixes_with_not_equal_size)
-{
-	TDynamicMatrix<int> m1(2), m2(3);
-	ASSERT_ANY_THROW(m1 - m2);
-}
-
-TEST(TDynamicMatrix, can_multiply_matrices_with_equal_size)
-{
-	TDynamicMatrix<int> m1(2), m2(2);
-	m1[0][0] = 1; m1[0][1] = 2;
-	m1[1][0] = 3; m1[1][1] = 4;
-
-	m2[0][0] = 5; m2[0][1] = 6;
-	m2[1][0] = 7; m2[1][1] = -8;
-
-	TDynamicMatrix<int> res(2);
-
-	res[0][0] = 19; res[0][1] = -10;
-	res[1][0] = 43; res[1][1] = -14;
-	EXPECT_EQ(m1 * m2, res);
-
-}
-
-*/
