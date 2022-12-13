79dd5a5823263df57d34a84c6b417ad16746c8b4
4mada0 <4mada0@gmail.com>
2022-12-13 14:13:27 +0300

    fixed repacking

--- include/tqueue.h
+++ include/tqueue.h
@@ -106,7 +106,10 @@ public:
 			int newSize = (memSize + 1) * COEFF;
 			T* tmp = new T[newSize];
 			memSize = newSize;
-			std::copy(pMem, pMem + sz, tmp);
+			for (int i = 0; i < sz; i++)
+			{
+				tmp[i] = *(pMem + (head - pMem + i) % memSize);
+			}
 			if(pMem) delete[] pMem;
 			pMem = tmp;
 			head = pMem;
